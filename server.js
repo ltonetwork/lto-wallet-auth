0a8c7ad47603f0e86237751fcc3a24638596cb02
arnold <arnold@jasny.net>
2022-11-11 11:57:37 -0400

    Fixup

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function clientError(res, status, message) {
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.status(200).header('Content-Type: application/json').write(JSON.stringinfy({
+    res.status(200).header('Content-Type: application/json').write(JSON.stringify({
         name: "LTO Wallet Auth",
         network: lto.network
     }));
